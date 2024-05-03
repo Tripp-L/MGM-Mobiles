470175436a427c21f35f4b40738855473d1124f3
trippl <tlaursen8@yahoo.com>
2024-05-03 11:11:46 -0400

    CarList/ Header 1

---
+++ motorgoat-mobiles-mgm/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>MotorGOAT-Mobiles</h1>
+            <img src="https://img.freepik.com/premium-photo/funny-goats-driving-with-cargenerative-ai_847296-3673.jpg" alt="Logo" />
+        </header>
+    )
+}
+
+export default Header;
