9ed7aed003451fab5c0e9ac54a3d962e1032a6c2
trippl <tlaursen8@yahoo.com>
2024-05-03 17:56:10 -0400

    CarList useState/useEffect

--- motorgoat-mobiles-mgm/src/Components/CarList.js
+++ motorgoat-mobiles-mgm/src/Components/CarList.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CarCard from "./CarCard";
 
-function CarList({ car }) {
+function CarList() {
+    const[cars, setCars]= useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/listings")
+       .then(res => res.json())
+       .then(cars => setCars(cars))
+    }, [])
+
     return (
         <div className="car-list">
-            {car.map((car) => (
+            {cars.map((car) => (
                 <CarCard key={car.id} car={car} />
             ))}
         </div>
