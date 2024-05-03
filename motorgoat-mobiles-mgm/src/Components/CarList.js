041cf616b8d56cd327518f0f4ba24026ee20bfe1
trippl <tlaursen8@yahoo.com>
2024-05-03 11:13:15 -0400

    CarList/ Header 1

---
+++ motorgoat-mobiles-mgm/src/Components/CarList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CarCard from "./CarCard";
+
+function CarList({ car }) {
+    return (
+        <div className="car-list">
+            {car.map((car) => (
+                <CarCard key={car.id} car={car} />
+            ))}
+        </div>
+    )
+}
+
+export default CarList;
