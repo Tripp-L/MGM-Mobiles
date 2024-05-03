f81e7bbccd4cd77d0ea23b437b54a5f2a030d08c
trippl <tlaursen8@yahoo.com>
2024-05-03 17:26:13 -0400

    db.json update

--- motorgoat-mobiles-mgm/src/Components/NewCarForm.js
+++ motorgoat-mobiles-mgm/src/Components/NewCarForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function NewCarForm({ addCar }) {
     const [formData, setFormData] = useState({
+        type: "",
         year: "",
         make: "",
         model: "",
@@ -18,6 +19,7 @@ function NewCarForm({ addCar }) {
         e.preventDefault()
         addCar(formData)
         setFormData({
+            type: "",
             year: "",
             make: "",
             model: "",
@@ -30,6 +32,7 @@ function NewCarForm({ addCar }) {
         <div className="new-car-form">
             <h2>Sell Your Car 🚗</h2>
             <form onSubmit={handleSubmit}>
+                <input type="text" name="type" placeholder="Type" value={formData.type} onChange={handleInputChange} />
                 <input type="text" name="year" placeholder="Year" value={formData.year} onChange={handleInputChange} />
                 <input type="text" name="make" placeholder="Make" value={formData.make} onChange={handleInputChange} />
                 <input type="text" name="model" placeholder="Model" value={formData.model} onChange={handleInputChange} />
