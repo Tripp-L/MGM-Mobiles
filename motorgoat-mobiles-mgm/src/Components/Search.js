324f6b4a4e89318493e5b6c2f67e75265af82079
trippl <tlaursen8@yahoo.com>
2024-05-03 14:05:40 -0400

    Search/ NewCarForm 1

---
+++ motorgoat-mobiles-mgm/src/Components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Search({ setSearchTerm }) {
+    function handleInputChange(e) {
+        setSearchTerm(e.target.value)
+    }
+
+    return (
+        <div className="search-bar">
+            <label htmlFor="search">Search 🔍</label>
+            <input type="text" id="search" placeholder="Start typing to search..." onChange={handleInputChange} />
+        </div>
+    )
+}
+
+export default Search;
