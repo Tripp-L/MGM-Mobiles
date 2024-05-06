8cf62053acb0557223ae5a7872598be5173f3cff
trippl <tlaursen8@yahoo.com>
2024-05-06 11:22:59 -0400

    Solved Server Issue

--- motorgoat-mobiles-mgm/src/Components/CarCard.js
+++ motorgoat-mobiles-mgm/src/Components/CarCard.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
-function CarCard({ car }) {
-  const [ id, type, year, make, model, image, price ] = car
+function CarCard({ type, year, make, model, image, price }) {
   const [ available, setAvailable ] = useState(true)
 
   function handleAvailableClick() {
@@ -9,10 +8,13 @@ function CarCard({ car }) {
   }
 
   return (
-    <li className="card" data-testid={`car-item-${id}`} >
-      <img src={image} alt={`${make} ${model}`} />
-      <h4>{`${year} ${make} ${model}`} </h4>
+    <li className="card" data-testid={"car-item"} >
+      <img src={image} alt={type} />
+      <h4>{type} </h4>
       <p>Type: {type}</p>
+      <p>Year: {year}</p>
+      <p>Make: {make}</p>
+      <p>Model: {model}</p>
       <p>Price: {price}</p>
       {available ? (
         <button onClick={handleAvailableClick} className="primary">Available!</button>
