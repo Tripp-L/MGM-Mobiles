eed64bd982df19cad2d177623ec8931c85840953
zachary oliver <zspoliver@gmail.com>
2024-05-06 19:14:18 -0400

    messed with Header and Navbar

--- motorgoat-mobiles-mgm/src/Components/Header.js
+++ motorgoat-mobiles-mgm/src/Components/Header.js
@@ -10,15 +10,6 @@ function Header({ setSearchTerm }) {
             <h1>MotorGOAT-Mobiles</h1>
             <img src="https://img.freepik.com/premium-photo/funny-goats-driving-with-cargenerative-ai_847296-3673.jpg" alt="Logo" />
             {location.pathname !== '/new-car' && <Search setSearchTerm={setSearchTerm} />}
-            <nav>
-                <ul>
-                    {location.pathname !== "/" && <li><Link to="/">Home</Link></li>}
-                    <li><Link to="/car">Cars</Link></li>
-                    <li><Link to="/truck">Trucks</Link></li>
-                    <li><Link to="/suv">SUVs</Link></li>
-                    <li><Link to="/new-car">Sell Your Car Here!!</Link></li>
-                </ul>
-            </nav>
         </header>
     )
 }
