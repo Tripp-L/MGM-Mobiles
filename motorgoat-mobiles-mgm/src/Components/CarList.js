c6d453ac64166f93eda33298446b43c61cdeadad
zachary oliver <zspoliver@gmail.com>
2024-05-06 11:59:15 -0400

    commit before pull 11:58

--- motorgoat-mobiles-mgm/src/Components/CarList.js
+++ motorgoat-mobiles-mgm/src/Components/CarList.js
@@ -2,19 +2,7 @@
 import React, { useState, useEffect  } from "react";
 import CarCard from "./CarCard";
 
-<<<<<<< HEAD
-function CarList({ car }) {
-    const [cars, setCars] = useState([])
-
-    useEffect(() => {
-        fetch("http://localhost:3000/listings")
-       .then((res) => res.json())
-       .then((data) => setCars(data.cars))
-    }, [])
-
-=======
 function CarList({ listings }) {
->>>>>>> refs/remotes/origin/Zack
 
     return (
         <ul className="car-list" >
