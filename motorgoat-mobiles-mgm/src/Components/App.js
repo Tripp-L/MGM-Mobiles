4ac6c43f1223dd3c262ec3e1ff8d3eab6a6ede95
trippl <tlaursen8@yahoo.com>
2024-05-07 14:08:47 -0400

    Code- nothing rendering again- Fix 5/7

--- motorgoat-mobiles-mgm/src/Components/App.js
+++ motorgoat-mobiles-mgm/src/Components/App.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Header";
 import CarPage from "./CarPage";
-import CarList from "./CarList"; 
 import NewCarForm from "./NewCarForm";
 import Navbar from "./Navbar"; 
+import CarCard from "./CarCard";
 
 function App() {
     const [listings, setListings] = useState([])
@@ -13,9 +13,18 @@ function App() {
     useEffect(() => {
         fetch("http://localhost:3001/listings")
             .then((res) => res.json())
-            .then((data) => setListings(data.listings)) 
+            .then((data) => setListings(data))
+
             .catch((error) => console.error("Error fetching data:", error))
-    }, [])
+    }, [searchTerm])
+
+    const filteredListings = listings.filter(car =>
+        car.type.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        car.make.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        car.model.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        car.year.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        car.price.toLowerCase().includes(searchTerm.toLowerCase())
+    )
 
     return (
         <Router>
@@ -23,9 +32,9 @@ function App() {
                 <Header setSearchTerm={setSearchTerm} /> 
                 <Navbar /> 
                 <Routes>
-                    <Route path="/cars" element={<CarList listings={listings.filter(car => car.type === 'Car')} />} />
-                    <Route path="/trucks" element={<CarList listings={listings.filter(car => car.type === 'Truck')} />} />
-                    <Route path="/suv" element={<CarList listings={listings.filter(car => car.type === 'SUV')} />} />
+                    <Route path="/cars" element={<CarCard listings={filteredListings.filter(car => car.make === 'Car')} />} />
+                    <Route path="/trucks" element={<CarCard listings={filteredListings.filter(car => car.make === 'Truck')} />} />
+                    <Route path="/suv" element={<CarCard listings={filteredListings.filter(car => car.make === 'SUV')} />} />
                     <Route path="/new-car" element={<NewCarForm setListings={setListings} />} />
                     <Route path="/" element={<CarPage />} />
                 </Routes>
@@ -35,6 +44,9 @@ function App() {
 }
 
 export default App;
+
+
+
 
 
 
