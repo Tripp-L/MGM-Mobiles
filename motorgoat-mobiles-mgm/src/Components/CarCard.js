a9ccbba3eef0f3892d4842a4a4a4dc0d9e59a336
zachary oliver <zspoliver@gmail.com>
2024-05-08 19:47:40 -0400

    found our buttongit add ..

--- motorgoat-mobiles-mgm/src/Components/CarCard.js
+++ motorgoat-mobiles-mgm/src/Components/CarCard.js
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
-import Container from 'react-bootstrap/Container';
+// import React, { useState } from "react";
+// import Container from 'react-bootstrap/Container';
 
-function CarCard({ type, year, make, model, image, price }) {
-  const [ available, setAvailable ] = useState(true)
+// function CarCard({ type, year, make, model, image, price }) {
+//   const [ available, setAvailable ] = useState(true)
 
-  function handleAvailableClick() {
-    setAvailable((currentAvailableState) => !currentAvailableState)
-  }
+//   function handleAvailableClick() {
+//     setAvailable((currentAvailableState) => !currentAvailableState)
+//   }
 
-  return (
-    <Container className="card" data-testid={"car-item"} >
-        <p>
-        {available ? (
-          <button onClick={handleAvailableClick} className="primary">Available!</button>
-      ) : (
-          <button onClick={handleAvailableClick} className="secondary">Sold!</button> 
-      )}
-        </p>
-        <img src={image} alt={type} />
-        <p>{year} {make}</p>
-        <p>{model}</p>
-        <p>Price: {price}</p>
-      </Container>
-  );
-}
+//   return (
+//     <Container className="card" data-testid={"car-item"} >
+//         <div>
+//         {available ? (
+//           <button onClick={handleAvailableClick} className="primary">Available!</button>
+//       ) : (
+//           <button onClick={handleAvailableClick} className="secondary">Sold!</button> 
+//       )}
+//         </div>
+//         <img src={image} alt={type} />
+//         <p>{year} {make}</p>
+//         <p>{model}</p>
+//         <p>Price: {price}</p>
+//       </Container>
+//   );
+// }
 
-export default CarCard;
+// export default CarCard;
--- motorgoat-mobiles-mgm/src/Components/CarList.js
+++ motorgoat-mobiles-mgm/src/Components/CarList.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "../App.css";
 
 function CarList({ listings }) {
+  const [ available, setAvailable ] = useState(true)
+
+  function handleAvailableClick() {
+    setAvailable((currentAvailableState) => !currentAvailableState)
+  }
+
   return (
     <div className="listings-container">
       {listings && Array.isArray(listings) && listings.map((listing, index) => (
         <div className="listing" key={index}>
+          <div>
+        {available ? (
+          <button style={{ color: 'black', backroundColor: 'green' }} onClick={handleAvailableClick} className="primary">Available!</button>
+      ) : (
+          <button  onClick={handleAvailableClick} className="secondary">Sold!</button> 
+      )}
+          </div>
           <img src={listing.image} alt={listing.type} />
           <p>{listing.year} {listing.make}</p>
           <p>{listing.model}</p>
